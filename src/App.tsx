00fb375b051b54715324db407796cdbb073b5944
Denis Kalkopf <ddenproton@protonmail.com>
2025-01-18 22:15:49 +0500

    add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { Input } from "@/components/ui/input";
 import { Canvas } from "@react-three/fiber";
 import Cube from "./components/Cube/Cube";
 import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/ModeToggle/ModeToggle";
 
 function App() {
   const [width, setWidth] = useState("1");
@@ -32,74 +34,79 @@ function App() {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-    setWidth(values.width);
-    setHeight(values.height);
-    setDepth(values.depth);
+    setTimeout(() => {
+      setWidth(values.width);
+      setHeight(values.height);
+      setDepth(values.depth);
+    }, 2000);
   }
 
   return (
-    <div className="h-screen grid sm:grid-cols-3">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 bg-gray-900 p-8"
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-screen grid sm:grid-cols-3">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 p-8 text-right"
+          >
+            <div className="flex justify-between items-center">
+              <h1 className="text-xl font-bold">CubeGen</h1>
+              <ModeToggle />
+            </div>
+            <FormField
+              control={form.control}
+              name="height"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Height</FormLabel>
+                  <FormControl>
+                    <Input placeholder="height" {...field} type="number" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="width"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Width</FormLabel>
+                  <FormControl>
+                    <Input placeholder="width" {...field} type="number" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="depth"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Depth</FormLabel>
+                  <FormControl>
+                    <Input placeholder="length" {...field} type="number" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Calculate</Button>
+          </form>
+        </Form>
+        <div
+          id="canvas-container"
+          className="h-full sm:h-lvh sm:col-span-2 border-2"
         >
-          <FormField
-            control={form.control}
-            name="height"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-white">Height</FormLabel>
-                <FormControl>
-                  <Input placeholder="height" {...field} type="number" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="width"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-white">Width</FormLabel>
-                <FormControl>
-                  <Input placeholder="width" {...field} type="number" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="depth"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-white">Depth</FormLabel>
-                <FormControl>
-                  <Input placeholder="length" {...field} type="number" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="bg-slate-300 text-black">
-            Calculate
-          </Button>
-        </form>
-      </Form>
-      <div
-        id="canvas-container"
-        className="h-full sm:h-lvh sm:col-span-2 bg-slate-800"
-      >
-        <Canvas>
-          <directionalLight castShadow position={[3, 7, 10]} intensity={2} />
-          <directionalLight position={[0, -6, -10]} intensity={2} />
-          <Cube width={width} height={height} depth={depth} />
-        </Canvas>
+          <Canvas>
+            <directionalLight castShadow position={[3, 7, 10]} intensity={2} />
+            <directionalLight position={[0, -6, -10]} intensity={2} />
+            <Cube width={width} height={height} depth={depth} />
+          </Canvas>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
