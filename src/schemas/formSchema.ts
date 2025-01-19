f2b4345f001aa3415b843f8c16386aab4595719e
Denis Kalkopf <ddenproton@protonmail.com>
2025-01-19 22:36:23 +0500

    fix

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  height: z.string().min(1).max(100),
-  width: z.string().min(1).max(100),
-  depth: z.string().min(1).max(100),
+  height: z.string().min(1, { message: "Height is required" }),
+  width: z.string().min(1, { message: "Width is required" }),
+  depth: z.string().min(1, { message: "Depth is required" }),
 });
